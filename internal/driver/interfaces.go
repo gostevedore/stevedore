c3e80a565270dd939780b30c6db4bee6cc066ce4
Aleix Penella <aleix.penella@edreamsodigeo.com>
2021-12-19 17:16:25 +0100

    comments

--- internal/driver/interfaces.go
+++ internal/driver/interfaces.go
@@ -2,7 +2,7 @@ package driver
 
 import "context"
 
-// BuildDriverer
+// BuildDriverer interface defines which methods are used to build a docker image
 type BuildDriverer interface {
 	//Run(context.Context) error
 	Build(context.Context, BuildDriverOptions) error
