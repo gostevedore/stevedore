abd5ba78269ab7f512654706dae81cf809b03ca9
Aleix Penella <aleix.penella@edreamsodigeo.com>
2022-05-20 19:32:47 +0200

    create core packages

--- internal/core/ports/repository/builder.go
+++ internal/core/ports/repository/builder.go
@@ -1,13 +1,1 @@
 package repository
-
-import (
-	"context"
-
-	"github.com/gostevedore/stevedore/internal/core/domain/driver"
-	"github.com/gostevedore/stevedore/internal/core/domain/image"
-)
-
-// BuildDriverer interface defines which methods are used to build a docker image
-type BuildDriverer interface {
-	Build(context.Context, *image.Image, *driver.BuildDriverOptions) error
-}
