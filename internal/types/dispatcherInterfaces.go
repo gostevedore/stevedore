4938398f9a4846102974a433b84a21c93dad4f8f
Aleix Penella <aleix.penella@edreamsodigeo.com>
2022-05-24 21:47:45 +0200

    remove types

--- internal/types/dispatcherInterfaces.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// import "github.com/gostevedore/stevedore/internal/schedule"
-
-// // Dispatcher
-// type Dispatcher interface {
-// 	Start() error
-// 	Enqueue(schedule.Jobber)
-// }
--- internal/types/drivererInterfaces.go
+++
@@ -1,8 +0,0 @@
-package types
-
-// import "context"
-
-// // Driverer element
-// type Driverer interface {
-// 	Run(context.Context) error
-// }
