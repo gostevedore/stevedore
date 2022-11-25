0e60def4a355b7d38ec1adb472dc65beb89c3217
Aleix Penella <aleix.penella@gmail.com>
2022-11-25 11:14:09 +0100

    add cli get builders

---
+++ internal/infrastructure/cli/get/builders/getBuildersFlagOptions.go
@@ -0,0 +1,7 @@
+package builders
+
+// getBuildersFlagOptions is the options for the get builders command
+type getBuildersFlagOptions struct {
+	// Filter is a list of filters to apply to the output
+	Filter []string
+}
