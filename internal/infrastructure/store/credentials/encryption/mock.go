0bbc1fbe4f9d867c7f68d2a6dcf381de6c045c52
Aleix Penella <aleix.penella@gmail.com>
2023-12-09 16:01:18 +0100

    include go vet into the ci workflow

--- internal/infrastructure/store/credentials/encryption/mock.go
+++ internal/infrastructure/store/credentials/encryption/mock.go
@@ -10,17 +10,17 @@ func NewMockEncryption() *MockEncription {
 	return &MockEncription{}
 }
 
-func (e MockEncription) Encrypt(text string) (string, error) {
+func (e *MockEncription) Encrypt(text string) (string, error) {
 	args := e.Mock.Called(text)
 	return args.String(0), args.Error(1)
 }
 
-func (e MockEncription) Decrypt(ciphertext string) (string, error) {
+func (e *MockEncription) Decrypt(ciphertext string) (string, error) {
 	args := e.Mock.Called(ciphertext)
 	return args.String(0), args.Error(1)
 }
 
-func (e MockEncription) GenerateEncryptionKey() (string, error) {
+func (e *MockEncription) GenerateEncryptionKey() (string, error) {
 	args := e.Mock.Called()
 	return args.String(0), args.Error(1)
 }
