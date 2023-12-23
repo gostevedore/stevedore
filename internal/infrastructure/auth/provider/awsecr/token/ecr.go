18d0b9c21e5a5a67ab0198bea3b9c33a4b501cb6
Aleix Penella <aleix.penella@gmail.com>
2023-12-23 15:31:26 +0100

    add comments to explain why errors are not managed

--- internal/infrastructure/auth/provider/awsecr/token/ecr.go
+++ internal/infrastructure/auth/provider/awsecr/token/ecr.go
@@ -52,45 +52,42 @@ func WithECRClientFactory(ecrClientFactory *ECRClientFactory) OptionsFunc {
 	}
 }
 
-func defaultCfgFunc(ctx context.Context, options ...func(*config.LoadOptions) error) (aws.Config, error) {
+func defaultAWSConfigFunc(ctx context.Context, options ...func(*config.LoadOptions) error) (aws.Config, error) {
 
-	errContext := "(token::AWSECRToken::defaultCfgFunc)"
-
-	cfg, err := config.LoadDefaultConfig(ctx, options...)
-	if err != nil {
-		errors.New(errContext, "", err)
-	}
+	// if LoadDefaultConfig returns an error the process continues with out returning an error. It is prefered to return nil and let the caller to decide what to do. In worst case scenario, docker API will return an error when trying to pull or push the image because the authorization token is not found
+	// TODO: log the error
+	cfg, _ := config.LoadDefaultConfig(ctx, options...)
 
 	return cfg, nil
 }
 
 // Get return the authorization token
-func (t *AWSECRToken) Get(ctx context.Context, cfgFunc func(context.Context, ...func(*config.LoadOptions) error) (aws.Config, error), credential *credentials.Credential) (*ecr.GetAuthorizationTokenOutput, error) {
+func (token *AWSECRToken) Get(ctx context.Context, AWSConfigFunc func(context.Context, ...func(*config.LoadOptions) error) (aws.Config, error), credential *credentials.Credential) (*ecr.GetAuthorizationTokenOutput, error) {
 
 	errContext := "(token::AWSECRToken::Token)"
-
-	if cfgFunc == nil {
-		cfgFunc = defaultCfgFunc
-	}
 
 	if credential == nil {
 		return nil, errors.New(errContext, "To get an ECR authorization token, you must provide a credential")
 	}
 
-	options, err := t.awsConfigLoadOptions(credential)
+	if AWSConfigFunc == nil {
+		AWSConfigFunc = defaultAWSConfigFunc
+	}
+
+	options, err := token.getAWSConfigLoadOptionFuncs(credential)
 	if err != nil {
 		return nil, errors.New(errContext, "", err)
 	}
 
-	cfg, err := cfgFunc(ctx, options...)
+	AWSConfig, err := AWSConfigFunc(ctx, options...)
 	if err != nil {
 		return nil, errors.New(errContext, "", err)
 	}
 
-	credentialsProvider, err := t.resolveCredentialsProvider(cfg, credential, options...)
-	if err != nil {
-		errors.New(errContext, "", err)
-	}
+	// The error is not being controled because is prefered to return nil and let the caller to decide what to do. AWS would be required for very specific cases and controlling the error would cause anoying errors to the user
+	// In worst case scenario, docker API will return an error when trying to pull or push the image because the authorization token is not found
+	// TODO: log the error
+	credentialsProvider, _ := token.resolveCredentialsProvider(AWSConfig, credential, options...)
 
 	if credentialsProvider != nil {
 		// when resolve credentials provider returns an empty aws.CredentialsCache it means that no credentials provider was found
@@ -99,22 +96,23 @@ func (t *AWSECRToken) Get(ctx context.Context, cfgFunc func(context.Context, ...
 			return nil, nil
 		}
 
-		cfg.Credentials = aws.NewCredentialsCache(credentialsProvider)
+		// configure the AWSConfig with the credentials provider
+		AWSConfig.Credentials = aws.NewCredentialsCache(credentialsProvider)
 	}
 
-	client := t.ecrClientFactory.Client(cfg)
-	auth, err := client.GetAuthorizationToken(ctx, &ecr.GetAuthorizationTokenInput{})
-	if err != nil {
-		errors.New(errContext, "", err)
-	}
+	client := token.ecrClientFactory.Client(AWSConfig)
+	// The error is not being controled because is prefered to return nil and let the caller to decide what to do. AWS would be required for very specific cases and controlling the error would cause anoying errors to the user
+	// In worst case scenario, docker API will return an error when trying to pull or push the image because the authorization token is not found
+	// TODO: log the error
+	auth, _ := client.GetAuthorizationToken(ctx, &ecr.GetAuthorizationTokenInput{})
 
 	return auth, nil
 }
 
-// awsConfigLoadOptions returns a list of load options to use when loading the default config.
-func (t *AWSECRToken) awsConfigLoadOptions(credential *credentials.Credential) ([]func(*config.LoadOptions) error, error) {
+// getAWSConfigLoadOptionFuncs returns a list of functions to configure the default config
+func (token *AWSECRToken) getAWSConfigLoadOptionFuncs(credential *credentials.Credential) ([]func(*config.LoadOptions) error, error) {
 
-	errContext := "(token::AWSECRToken::awsConfigLoadOptions)"
+	errContext := "(token::AWSECRToken::getAWSConfigLoadOptionFuncs)"
 
 	if credential == nil {
 		return nil, errors.New(errContext, "To get an ECR authorization token, you must provide a credential")
@@ -142,7 +140,7 @@ func (t *AWSECRToken) awsConfigLoadOptions(credential *credentials.Credential) (
 }
 
 // resolveCredentialsProvider returns the credentials provider to use for the given config. To use the default aws configuration, is returned a nil provider. When no provider is found it is returned an empty CredentialsCache.
-func (t *AWSECRToken) resolveCredentialsProvider(cfg aws.Config, credential *credentials.Credential, options ...func(*config.LoadOptions) error) (aws.CredentialsProvider, error) {
+func (token *AWSECRToken) resolveCredentialsProvider(cfg aws.Config, credential *credentials.Credential, options ...func(*config.LoadOptions) error) (aws.CredentialsProvider, error) {
 	var provider aws.CredentialsProvider
 	var err error
 	errContext := "(token::AWSECRToken::resolveCredentialsProvider)"
@@ -152,9 +150,9 @@ func (t *AWSECRToken) resolveCredentialsProvider(cfg aws.Config, credential *cre
 	}
 
 	if credential.AWSRoleARN != "" {
-		if t.assumeRoleARNProvider != nil {
+		if token.assumeRoleARNProvider != nil {
 
-			provider, err = t.assumeRoleARNProvider.CredentialsProvider(cfg, credential.AWSRoleARN, credential.AWSAccessKeyID, credential.AWSSecretAccessKey, "", options...)
+			provider, err = token.assumeRoleARNProvider.CredentialsProvider(cfg, credential.AWSRoleARN, credential.AWSAccessKeyID, credential.AWSSecretAccessKey, "", options...)
 			if err != nil {
 				return nil, errors.New(errContext, "", err)
 			}
@@ -164,8 +162,8 @@ func (t *AWSECRToken) resolveCredentialsProvider(cfg aws.Config, credential *cre
 	}
 
 	if credential.AWSAccessKeyID != "" && credential.AWSSecretAccessKey != "" {
-		if t.staticCredentialsProvider != nil {
-			provider, err = t.staticCredentialsProvider.CredentialsProvider(credential.AWSAccessKeyID, credential.AWSSecretAccessKey, "", options...)
+		if token.staticCredentialsProvider != nil {
+			provider, err = token.staticCredentialsProvider.CredentialsProvider(credential.AWSAccessKeyID, credential.AWSSecretAccessKey, "", options...)
 			if err != nil {
 				return nil, errors.New(errContext, "", err)
 			}
@@ -175,6 +173,7 @@ func (t *AWSECRToken) resolveCredentialsProvider(cfg aws.Config, credential *cre
 	}
 
 	if credential.AWSUseDefaultCredentialsChain {
+		// return a nil provider to use the default aws configuration
 		return nil, nil
 	}
 
