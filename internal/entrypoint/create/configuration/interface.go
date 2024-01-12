8335c7aa2774cc1a40438d04871b9a82ca9121d9
Aleix Penella <aleix.penella@gmail.com>
2024-01-12 07:42:13 +0100

    notify user that configuration has been created

--- internal/entrypoint/create/configuration/interface.go
+++ internal/entrypoint/create/configuration/interface.go
@@ -1,5 +1,19 @@
 package configuration
 
+import "io"
+
 type EncryptionKeyGenerator interface {
 	GenerateEncryptionKey() (string, error)
+}
+
+type Consoler interface {
+	io.Writer
+	ConsoleWriter
+}
+
+type ConsoleWriter interface {
+	Info(msg ...interface{})
+	Warn(msg ...interface{})
+	Error(msg ...interface{})
+	Debug(msg ...interface{})
 }
